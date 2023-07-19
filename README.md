a16a687a1790b7f11b5d273a907a20a8977bc584
Tomisin Jenrola <13075885+ToJen@users.noreply.github.com>
2023-07-19 13:16:18 -0400

    [CU-868501426] refactor: use PromptBuilder for LLMSkill (#2)
    * [CU-868501426] refactor: use PromptBuilder for LLMSkill
    
    * update README
    
    * fix jinja templates, references and context data

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # web3-copilot
-Web3Copilot is artificial intelligence tool developed using [CouncilAI by ChainML](https://github.com/chain-ml/council) to assist Web3 users navigate the ecosystem.
+Web3Copilot is an artificial intelligence tool developed using [CouncilAI by ChainML](https://github.com/chain-ml/council) to assist Web3 users navigate the ecosystem.
 
 ## Requirements
 - Python >=3.9
--- agent.py
+++ agent.py
@@ -1,7 +1,7 @@
 from typing import List
-from string import Template
-import toml
+from pathlib import Path
 
+import toml
 import dotenv
 
 from council.agents import Agent
@@ -9,7 +9,8 @@ from council.chains import Chain
 from council.contexts import AgentContext, ChainContext, ChatHistory
 from council.runners.budget import Budget
 from council.llm import OpenAILLMConfiguration, OpenAILLM, LLMMessage
-from council.skills import LLMSkill
+from council.skills import LLMSkill, PromptToMessages
+from council.prompt import PromptBuilder
 from council.controllers import LLMController
 from council.evaluators import LLMEvaluator
 
@@ -19,8 +20,8 @@ from retrieval import Retriever
 from skills import DocRetrievalSkill
 from utils import create_file_dict
 
-
 dotenv.load_dotenv()
+
 
 class DocRetrievalAgent:
 
@@ -35,22 +36,13 @@ class DocRetrievalAgent:
         self.retriever = Retriever(self.config)
 
         # Initialize agent
-        self.context = AgentContext(chat_history=ChatHistory()) 
+        self.context = AgentContext(chat_history=ChatHistory())
         self.llm = OpenAILLM(config=OpenAILLMConfiguration.from_env())
-        self.load_prompts()
         self.init_skills()
         chains = self.init_chains()
         self.controller = LLMController(llm=self.llm, top_k_execution_plan=1)
         self.evaluator = LLMEvaluator(self.llm)
         self.agent = Agent(self.controller, chains, self.evaluator)
-
-    def load_prompts(self):
-        self.system_prompt = toml.load(
-            "./templates/doc_retrieval_prompt.toml"
-        )['system_message']['prompt']
-        self.prompt_template = Template(toml.load(
-            "./templates/doc_retrieval_prompt.toml")['main_prompt']['prompt']
-                                        )
 
     def init_skills(self):
         # Skills for document retrieval
@@ -67,7 +59,7 @@ class DocRetrievalAgent:
         # Skill to interact with LLM
         self.llm_skill = LLMSkill(
             llm=self.llm,
-            system_prompt=self.system_prompt,
+            system_prompt=self.load_system_prompt(),
             context_messages=self.build_context_message
         )
 
@@ -86,15 +78,19 @@ class DocRetrievalAgent:
             )
         return chains
 
+    def load_system_prompt(self):
+        system_prompt = Path("./templates/doc_retrieval_prompt.jinja").read_text()
+        return system_prompt
+
     def build_context_message(self, context: ChainContext) -> List[LLMMessage]:
-        doc_context = context.current.last_message().unwrap().data
-        query = context.chatHistory.last_user_message().unwrap().message     
+        context_message_template = Path("./templates/context_message_prompt.jinja").read_text()
+        context_message_prompt = PromptToMessages(
+            prompt_builder=PromptBuilder(context_message_template)
+        )
 
-        prompt = self.prompt_template.substitute(context=doc_context, query=query)
-
-        return [LLMMessage.user_message(prompt)]
+        return context_message_prompt.to_user_message(context)
 
     def interact(self, message):
         self.context.chatHistory.add_user_message(message)
-        result = self.agent.execute(context=self.context, budget=Budget(60))
+        result = self.agent.execute(context=self.context, budget=Budget(100))
         return result
